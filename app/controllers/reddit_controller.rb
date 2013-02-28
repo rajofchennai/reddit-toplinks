27180eb69df14cbb9d9b1b6dbf41e7f156a94243
rajofchennai <rajofchennai@yahoo.com>
2013-02-28 23:25:23 +0530

    Controller spec

--- app/controllers/reddit_controller.rb
+++ app/controllers/reddit_controller.rb
@@ -9,10 +9,18 @@ class RedditController < ApplicationController
     parameters = "limit=#{PAGE_SIZE}"
     parameters << "&after=#{params[:after]}" if params[:after]
     parameters << "&before=#{params[:before]}" if params[:before]
-    @page = 1
-    @page = params[:page].to_i if params[:page]
+
+    # params of page is mainly to track the current page
+    if params[:page].to_i == 0
+      @page = 1
+    else
+      @page = params[:page].to_i
+    end
+
     if @page <= MAX_PAGES && @page > 0
       @feeds = JSON.parse(RestClient.get("http://www.reddit.com/top.json?#{parameters}"))['data']['children']
+      @previous_page = "/reddit/top_url?before=#{@feeds[0]['data']['name']}&page=#{@page-1}"
+      @next_page = "/reddit/top_url?after=#{@feeds.last['data']['name']}&page=#{@page+1}"
     end
   end
 end
