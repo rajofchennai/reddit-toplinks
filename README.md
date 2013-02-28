3d8c7aedf4c63c248915b1c34f722ba238c7a2e5
rajofchennai <rajofchennai@yahoo.com>
2013-02-28 23:51:28 +0530

    update documentation and more routing specs

--- README.md
+++ README.md
@@ -2,3 +2,26 @@ reddit-toplinks
 ===============
 
 Best place to get top stories on reddit
+
+setup
+=====
+
+rvm install ruby-1.9.3-p362
+rvm use ruby-1.9.3-p362
+rvm gemset create reddit
+rvm gemset use reddit
+gem install bundler
+bundle install
+bundle exec rake db:migrate
+
+Specs
+=====
+
+bundle exec rspec
+
+Running the app
+===============
+
+bundle exec rails s
+
+Default port: 3000
--- spec/routing/route_spec.rb
+++ spec/routing/route_spec.rb
@@ -10,9 +10,15 @@ describe "Routes" do
     {:get => '/reddit/top_url'}.should route_to({:controller => 'reddit', :action => 'top_url'})
   end
 
-  it "all non-get requests should route to application#error_routing" do
+  it "all non-get invalid requests should route to application#error_routing" do
     {:post => '/'}.should route_to({:controller => 'application', :action => 'error_routing'})
     {:delete => '/abc'}.should route_to({:controller => 'application', :action => 'error_routing', :anything => 'abc'})
     {:post => '/reddit/top_url'}.should route_to({:controller => 'application', :action => 'error_routing', :anything => 'reddit/top_url'})
+  end
+
+  it "login screen and signin" do
+    {:get => '/login'}.should route_to({:controller => 'session', :action => 'login'})
+    {:post => '/signin', :email => 'abc'}.should route_to({:controller => 'session', :action => 'signin'})
+    {:get => '/logout'}.should route_to({:controller => 'session', :action => 'logout'})
   end
 end
